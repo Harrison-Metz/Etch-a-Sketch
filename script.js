3c13f8203ffaa91e5a94ea24a264bc043ab7f4e8
Harrison Metz <harrisonmetz17@gmail.com>
2025-07-15 11:58:50 -0800

    refactored functions to make them cleaner

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 const container = document.querySelector('.grid-container');
 
-function createDiv(){
-    const gridBlock = document.createElement('div');
-    gridBlock.classList.add('grid');
-    container.appendChild(gridBlock);
-
-}
-
-function createRow(index){
-    const gridRow = document.createElement('div');
-    gridRow.classList.add('row' + index);
-    container.appendChild(gridRow);
-    
-}
-
-function createGrid(width, height){
-    for(let i = 0; i < height; i++){
-        for(let j = 0; j < width; j++){
-            createDiv(j);
-            console.log('DIV: ' + j + ' ROW: ' + i);
-        }
-        const lineBreak = document.createElement('br');
-        container.appendChild(lineBreak);
+function createRow(columns){
+    const row = document.createElement('div');
+    row.className = 'row';
+    container.appendChild(row);
+    for(let i = 0; i < columns; i++){
+        const box = document.createElement('div');
+        box.className = 'box';
+        row.appendChild(box);
     }
-    
+}
+
+function createGrid(rows, columns){
+    for(let i = 0; i < rows; i++){
+        createRow(columns);
+    }
 }
 
 createGrid(16, 16);
