0c54afd5a14949467ec2d8861cf6cbe7b583abc6
Harrison Metz <harrisonmetz17@gmail.com>
2025-07-15 16:05:55 -0800

    add opacity gradient for how many times a box is hit.

--- script.js
+++ script.js
@@ -1,45 +1,5 @@
-let colors = [
-    {
-        numberOfTouches : 0,
-        color : 'rgb(255, 255, 255)'
-    },
-    {
-        numberOfTouches : 1,
-        color : 'rgb(227, 227, 227)'
-    },
-    {
-        numberOfTouches : 2, 
-        color : 'rgb(198, 198, 198)'
-    },
-    {
-        numberOfTouches : 3, 
-        color : 'rgb(170, 170, 170)'
-    },
-    {
-        numberOfTouches : 4,
-        color : 'rgb(142, 142, 142)'
-    },
-    {
-        numberOfTouches : 5,
-        color : 'rgb(113, 113, 113)'
-    },
-    {
-        numberOfTouches : 6,
-        color : 'rgb(85, 85, 85)'
-    },
-    {
-        numberOfTouches : 7,
-        color : 'rgb(57, 57, 57)'
-    },
-    {
-        numberOfTouches : 8,
-        color : 'rgb(28, 28, 28)'
-    },
-    {
-        numberOfTouches : 9,
-        color : 'rgb(0, 0, 0)'
-    },
-];
+let strokeInput = 1;
+let strokes = 1 / strokeInput;
 const container = document.querySelector('.grid-container');
 
 function createRow(columns){
@@ -50,7 +10,9 @@ function createRow(columns){
         const box = document.createElement('div');
         box.className = 'box';
         box.addEventListener('mouseover', function(){
-            box.style.backgroundColor = 'black';
+            let opacity = parseFloat(getComputedStyle(box).opacity)
+            let newOpacity = Math.max(0, opacity - strokes);
+            box.style.opacity = newOpacity;
         });
         row.appendChild(box);
     }
